d3ab03836526dfb2e0b2b42281e3739ce8f85802
SabirinMohamedAli <thaprinmohamett1333@gmail.com>
2024-10-27 22:59:28 +0300

    updated

--- index.php
+++ index.php
@@ -28,7 +28,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 <!-- Header -->
 <header>
-    <div class="logo">mahira</div>
+    <div class="logo">sabi</div>
     <ul>
         <li><a href="#about">About us</a></li>
         <li><a href="#services">Services</a></li>
